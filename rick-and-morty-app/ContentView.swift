332f336e784f9f5a2902fbae2d444177a6e2d947
Si <code@iPon.es>
2023-11-07 13:22:57 +0100

    created xcode project

---
+++ rick-and-morty-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  rick-and-morty-app
+//
+//  Created by Simón Aparicio on 07/11/2023.
+//  Copyright © 2023 iPon.es All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rick-and-morty-app/rick_and_morty_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  rick-and-morty-app
+//
+//  Created by Simón Aparicio on 07/11/2023.
+//  Copyright © 2023 iPon.es All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct rick_and_morty_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
