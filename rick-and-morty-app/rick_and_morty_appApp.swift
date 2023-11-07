4111bb6ab1727c0faf96f551d017bc0d50fa3ab0
Si <code@iPon.es>
2023-11-07 13:25:57 +0100

    fixed indentation

--- rick-and-morty-app/rick_and_morty_appApp.swift
+++ rick-and-morty-app/rick_and_morty_appApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct rick_and_morty_appApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
+
