448ac0a6f76c528e001d6fc898bfceed7331b490
Si <code@ipon.es>
2023-11-07 16:31:53 -0800

    added Visual Model for Character and Mapper

---
+++ rick-and-morty-app/Sources/Features/CharacterList/Models/Character.swift
@@ -0,0 +1,24 @@
+//
+//  rick-and-morty-app
+//
+//  Created by Simón Aparicio on 08/11/2023.
+//  Copyright © 2023 iPon.es All rights reserved.
+//
+
+import Foundation
+
+struct Character {
+
+    let id,
+        name,
+        status,
+        species,
+        type,
+        gender,
+        origin,
+        location,
+        image,
+        numberOfEpisodes,
+        created: String
+}
+
