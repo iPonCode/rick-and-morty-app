420b6d746657984c22064f0bf1e44aa1c48dc2b1
Si <code@ipon.es>
2023-11-08 16:49:04 -0800

    added Ubuntu Font Family

---
+++ rick-and-morty-app/Resorces/Fonts/UbuntuFont.swift
@@ -0,0 +1,35 @@
+//
+//  rick-and-morty-app
+//
+//  Created by Simón Aparicio on 08/11/2023.
+//  Copyright © 2023 iPon.es All rights reserved.
+//
+
+import SwiftUI
+
+enum UbuntuFont: String, CaseIterable {
+
+	case regular = "Ubuntu-Regular",
+       italic = "Ubuntu-Italic",
+       light = "Ubuntu-Light",
+       lightItalic = "Ubuntu-LightItalic",
+       medium = "Ubuntu-Medium",
+       mediumItalic = "Ubuntu-MediumItalic",
+       bold = "Ubuntu-Bold",
+       boldItalic = "Ubuntu-BoldItalic"
+}
+
+extension Font {
+
+	static func ubuntu(
+		_ type: UbuntuFont,
+		size: CGFloat
+	) -> Font {
+
+		.custom(
+      type.rawValue,
+      size: size
+    )
+	}
+}
+
---
+++ rick-and-morty-app/Sources/UI/Common/Fonts/UbuntuAppFont.swift
@@ -0,0 +1,60 @@
+//
+//  rick-and-morty-app
+//
+//  Created by Simón Aparicio on 08/11/2023.
+//  Copyright © 2023 iPon.es All rights reserved.
+//
+
+import SwiftUI
+
+enum UbuntuAppFont {
+	case regular28,
+			 regular22,
+			 regular18,
+			 regular16,
+			 regular14,
+       medium16,
+       medium20,
+       bold17,
+       bold22
+
+	var type: UbuntuFont {
+		switch self {
+      case .regular28,
+           .regular22,
+           .regular18,
+           .regular16,
+           .regular14: return .regular
+      case .medium16,
+           .medium20: return .medium
+      case .bold17,
+           .bold22: return .bold
+		}
+	}
+
+	var size: CGFloat {
+		switch self {
+		case .regular28: return 28
+		case .regular22: return 22
+		case .regular18: return 18
+		case .regular16: return 16
+		case .regular14: return 14
+    case .medium16: return 16
+    case .medium20: return 20
+    case .bold17: return 17
+    case .bold22: return 22
+		}
+	}
+}
+
+extension Font {
+	static func ubuntuAppFont(
+		_ font: UbuntuAppFont
+	) -> Font {
+		ubuntu(
+			font.type,
+			size: font.size
+		)
+	}
+}
+
