170024fe6c6e790a4e4b014722c62829c7c3be27
Si <code@ipon.es>
2023-11-08 18:13:29 -0800

    WIP playing with ScrollView + LazyVStack instead a List

--- rick-and-morty-app/Sources/UI/CharacterListView.swift
+++ rick-and-morty-app/Sources/UI/CharacterListView.swift
@@ -12,46 +12,32 @@ struct CharacterListView: View {
 	@ObservedObject var viewModel: CharacterListViewViewModel
 
 	var body: some View {
-		List(
-			viewModel.list,
-			id: \.self
-		) { character in
-			NavigationLink(
-				destination: CharacterDetailView(
-					viewModel: CharacterDetailViewViewModel(
-            character: character.name
-					))) {
-            HStack(alignment: .center) {
-              AsyncImage(url: URL(string: character.image)) { image in
-                 image
-                     .resizable()
-                     .aspectRatio(contentMode: .fit)
-                     .frame(height: 112)
-             } placeholder: {
-               Rectangle()
-                 .fill(.clear)
-                 .frame(width: 112, height: 112)
-                 .overlay(alignment: .center) {
-                   ProgressView()
-                     .tint(.black)
-                 }
-             }
-              VStack(alignment: .leading, spacing: 4) {
-                Text(character.name)
-                  .font(.appFont(.regular22))
-                Text(character.status)
-                  .font(.ubuntuAppFont(.medium16))
-                  .foregroundColor(.purple)
-                Text(character.gender)
-                  .font(.ubuntuAppFont(.regular18))
-                  .foregroundColor(.brown)
-                Text(character.location)
-                  .font(.ubuntuAppFont(.regular16))
-                
+
+    ScrollView(.vertical) {
+
+      LazyVStack {
+
+        ForEach(
+          viewModel.list,
+          id: \.self
+        ) { character in
+
+          NavigationLink(
+            destination: CharacterDetailView(
+              viewModel: CharacterDetailViewViewModel(
+                character: character.name
+              ))) {
+                ItemView(character: character)
               }
-            }
-					}
-		}
+//          .scrollTransition { content, phase in
+//            content
+//              .scaleEffect(phase.isIdentity ? 1.0 : 0.7)
+//          }
+        }
+      }
+//      .safeAreaPadding(.vertical)
+    }
+    .scrollIndicators(.hidden)
 		.navigationTitle("Character List")
 	}
 
@@ -66,3 +52,47 @@ struct CharacterListView_Previews: PreviewProvider {
   }
 }
 
+struct ItemView: View {
+
+  let character: Character
+  var body: some View {
+    HStack(alignment: .center, spacing: 12) {
+      AsyncImage(url: URL(string: character.image)) { image in
+        image
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(height: 112)
+          .clipShape(RoundedRectangle(cornerRadius: 13.0))
+      } placeholder: {
+        Rectangle()
+          .fill(.clear)
+          .frame(width: 112, height: 112)
+          .overlay(alignment: .center) {
+            ProgressView()
+              .tint(.black)
+          }
+      }
+      VStack(alignment: .leading, spacing: 4) {
+        Text(character.name)
+          .font(.appFont(.regular28))
+          .foregroundColor(.primary)
+        
+        Group {
+          Text(character.status)
+            .foregroundColor(.purple) +
+          Text(" · ")
+            .foregroundColor(.gray)
+            .bold() +
+          Text(character.gender)
+            .foregroundColor(.indigo)
+        }
+        .font(.ubuntuAppFont(.medium16))
+        
+        Text(character.location)
+          .font(.ubuntuAppFont(.regular16))
+          .foregroundColor(.gray)
+      }
+      Spacer()
+    }
+  }
+}
